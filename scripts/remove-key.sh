d00bd48f7736a51de28ad6e4c9ec0b2d703a0d10
Rahul Patil <rahul11.victory@gmail.com>
2015-05-13 16:04:06 +0530

    Changed Script Files

--- scripts/remove-key.sh
+++ scripts/remove-key.sh
@@ -1,16 +1,24 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1347\cocoasubrtf570
-{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red121\green49\blue30;\red255\green255\blue255;\red26\green26\blue26;
-\red33\green152\blue210;\red232\green41\blue40;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl426
+#!/bin/sh
 
-\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 #!/bin/sh\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-security delete-keychain ios-build\cf5 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec5 .keychain\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-rm -f \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "~/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"}
+# Create a custom keychain
+security create-keychain -p travis ios-build.keychain
+
+# Make the custom keychain default, so xcodebuild will use it for signing
+security default-keychain -s ios-build.keychain
+
+# Unlock the keychain
+security unlock-keychain -p travis ios-build.keychain
+
+# Set keychain timeout to 1 hour for long builds
+# see http://www.egeek.me/2013/02/23/jenkins-and-xcode-user-interaction-is-not-allowed/
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
+
+# Add certificates to keychain and allow codesign to access them
+security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+
+
+# Put the provisioning profile in place
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+cp "./scripts/profile/$PROFILE_NAME.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -1,56 +1,14 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1347\cocoasubrtf570
-{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red121\green49\blue30;\red255\green255\blue255;\red26\green26\blue26;
-\red151\green0\blue126;\red232\green41\blue40;\red0\green0\blue153;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl426
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+echo "This is a pull request. No deployment will be done."
+exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+echo "Testing on a branch other than master. No deployment will be done."
+exit 0
+fi
 
-\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 #!/bin/sh\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-\pard\pardeftab720\sl426
-\cf5 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec5 if\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  [[ \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "$TRAVIS_PULL_REQUEST"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  != \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "false"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  ]]; then\
-  echo \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "This is a pull request. No deployment will be done."\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-  exit \cf7 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec7 0\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-fi\
-\cf5 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec5 if\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  [[ \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "$TRAVIS_BRANCH"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  != \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "master"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  ]]; then\
-  echo \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "Testing on a branch other than master. No deployment will be done."\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-  exit \cf7 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec7 0\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-fi\
-\
-PROVISIONING_PROFILE=\cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-OUTPUTDIR=\cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "$PWD/build/Release-iphoneos"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4 \
-\
-xcrun -log -sdk iphoneos PackageApplication \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "$OUTPUTDIR/$APP_NAME.app"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  -o \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "$OUTPUTDIR/$APP_NAME.ipa"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  -sign \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "$DEVELOPER_NAME"\cf4 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec4  -embed \cf6 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec6 "$PROVISIONING_PROFILE"}
+PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
+OUTPUTDIR="$PWD/build/Release-iphoneos"
+
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
